f7d1c8dbf13e5a8e1025cb01366667ea40aa972b
Giuszz <giuistired@gmail.com>
2025-02-07 22:34:07 +0000

    código semi completo (erro ppc/dpp)

--- Desafiosupertrunfo.c
+++ Desafiosupertrunfo.c
@@ -40,16 +40,68 @@ scanf ("%d", &pontosturisticos);
 float pibpercapita = pib / populacao;
 float densidadepop = areakm / populacao;
 
+float superpoder = areakm + populacao + pib + pontosturisticos;
+
+
 printf ("Estado: %s \n", estado);
 printf ("Código da carta: %s \n", codigodacarta);
 printf ("Nome da cidade: %s \n", nomecidade);
 printf ("População: %d \n", populacao);
-printf ("Área (Km²): %f \n", areakm);
-printf ("PIB: %f \n", pib);
+printf ("Área (Km²): %3.f \n", areakm);
+printf ("PIB: %2.f \n", pib);
 printf ("Pontos turísticos: %d \n", pontosturisticos);
-printf ("PIB per capita: %f \n", pib/populacao);
-printf ("Densidade populacional: %f \n", densidadepop/populacao);
+printf ("PIB per capita: %2.f \n", pib/populacao);
+printf ("Densidade populacional: %3.f \n", densidadepop/populacao);
+printf ("Superpoder: %3.f \n", superpoder);
 
+printf ("A primeira carta foi finalizada.\n");
+
+char estado2[20];
+char codigodacarta2 [20];
+char nomecidade2 [20];
+int populacao2;
+float areakm2;
+float pib2;
+int pontosturisticos2;
+
+printf ("Agora, insira os dados da segunda: \n");
+
+printf ("Insira o estado (sigla): \n");
+scanf ("%s", &estado2);
+
+printf ("Digite o código da carta: \n");
+scanf ("%s", &codigodacarta2);
+
+printf ("Digite o nome da cidade: \n");
+scanf ("%s", &nomecidade2);
+
+printf ("Digite a população da cidade: \n");
+scanf ("%d", &populacao2);
+
+printf ("Digite a área em km²: \n");
+scanf ("%f", &areakm2);
+
+printf ("Digite o PIB da cidade: \n");
+scanf ("%f", &pib2);
+
+printf ("Por último, digite o número de pontos turísticos: \n");
+scanf ("%d", &pontosturisticos2);
+
+float pibpercapita2 = pib2 / populacao2;
+float densidadepop2 = areakm2 / populacao2;
+
+float superpoder2 = areakm2 + pib2 + populacao2 + pontosturisticos2;
+
+printf ("Estado: %s \n", estado2);
+printf ("Código da carta: %s \n", codigodacarta2);
+printf ("Nome da cidade: %s \n", nomecidade2);
+printf ("População: %d \n", populacao2);
+printf ("Área (Km²): %3.f \n", areakm2);
+printf ("PIB: %2.f \n", pib2);
+printf ("Pontos turísticos: %d \n", pontosturisticos2);
+printf ("PIB per capita: %2.f \n", pib2/populacao2);
+printf ("Densidade populacional: %3.f \n", densidadepop2/populacao2);
+printf ("Superpoder: %3.f \n", superpoder2);
 
 
 
